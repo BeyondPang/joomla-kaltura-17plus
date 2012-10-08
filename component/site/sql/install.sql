abb1367f42ce8134bf01002719cbe85b2fb4d778
David Bezemer <dbz@stoas.nl>
2012-10-09 01:02:58 +0200

    Update UIconf Id's for CE5

--- component/site/sql/install.sql
+++ component/site/sql/install.sql
@@ -4,14 +4,14 @@ CREATE TABLE  `#__kaltura_config` (
   `value` text NOT NULL,
   PRIMARY KEY  (`name`)
 );
-INSERT INTO `#__kaltura_config` VALUES('player_regular_light','1011902');
-INSERT INTO `#__kaltura_config` VALUES('player_regular_dark','1011912');
-INSERT INTO `#__kaltura_config` VALUES('player_regular_light with share','1011932');
-INSERT INTO `#__kaltura_config` VALUES('player_regular_dark with share','1011922');
-INSERT INTO `#__kaltura_config` VALUES('player_mix_light','1011902');
-INSERT INTO `#__kaltura_config` VALUES('player_mix_dark','1011912');
-INSERT INTO `#__kaltura_config` VALUES('player_mix_light with share','1011932');
-INSERT INTO `#__kaltura_config` VALUES('player_mix_dark with share','1011922');
+INSERT INTO `#__kaltura_config` VALUES('player_regular_light','1347302');
+INSERT INTO `#__kaltura_config` VALUES('player_regular_dark','1347352');
+INSERT INTO `#__kaltura_config` VALUES('player_regular_light with share','1676202');
+INSERT INTO `#__kaltura_config` VALUES('player_regular_dark with share','1347462');
+INSERT INTO `#__kaltura_config` VALUES('player_mix_light','1433562');
+INSERT INTO `#__kaltura_config` VALUES('player_mix_dark','1435082');
+INSERT INTO `#__kaltura_config` VALUES('player_mix_light with share','1435152');
+INSERT INTO `#__kaltura_config` VALUES('player_mix_dark with share','1435202');
 INSERT INTO `#__kaltura_config` VALUES('server_uri','http://www.kaltura.com');
 INSERT INTO `#__kaltura_config` VALUES('editor','1002226');
 INSERT INTO `#__kaltura_config` VALUES('uploader_regular','1013682');
